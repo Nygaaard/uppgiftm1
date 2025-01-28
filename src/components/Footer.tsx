2f9cc33138e8f14263fb1651aa7db909f9c242ab
Nygaaard <nygardandreas@hotmail.com>
2025-01-28 18:28:39 +0100

    Created footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Utvecklare av denna webbplats är Andreas Nygård</p>
+        <p><a href="mailto:anny2301@student.miun.se" className="link" style={{color: "#fff"}}>anny2301@student.miun.se</a></p>
+    </footer>
+  )
+}
+
+export default Footer
